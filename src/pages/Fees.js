b5dc9d94049040083c869385d5d40bc2870c4cc1
Aries <contact@synergynodes.com>
2022-07-05 09:22:21 +0000

    Updated Contact Info

--- src/pages/Fees.js
+++ src/pages/Fees.js
@@ -8,11 +8,11 @@ return (
         <p>You can support Synergy Nodes by delegating your respective Tokens to any of our following Validator Nodes.</p>
 
 		<div className='nodes'>
-			<span className='node'><img src='../images/kujira.png'></img><a href=''>Kujira</a></span>
-			<span className='node'><img src='../images/luna.svg'></img><a href=''>Terra V2</a></span>
-			<span className='node'><img src='../images/provenance.svg'></img><a href=''>Provenance</a></span>
-			<span className='node'><img src='../images/nym.png'></img><a href=''>Nym Mixnode</a></span>
-			<span className='node'><img src='../images/oraichain.png'></img><a href=''>Oraichain</a></span>
+			<span className='node'><img src='../images/kujira.png'></img><a href='https://blue.kujira.app/stake/kujiravaloper1lcgzkqstk4jjtphfdfjpw9dd9yfczyzmcyxvj9' target='_blank'>Kujira</a></span>
+			<span className='node'><img src='../images/luna.svg'></img><a href='https://station.terra.money/validator/terravaloper1tm0xkark2ufecmdhgaaw7gqsycyt4qag6py8x6' target='_blank'>Terra V2</a></span>
+			<span className='node'><img src='../images/provenance.svg'></img><a href='https://explorer.provenance.io/validator/pbvaloper170h0kkjlphjk7qg46qx7uuatfpfpr0nh70kcq4' target='_blank'>Provenance</a></span>
+			<span className='node'><img src='../images/nym.png'></img><a href='https://mixnet.explorers.guru/mixnode/58VJVZJxJ19xGtaJH8GWCpWuczdCKHEEzQoYn2XUwdkU' target='_blank'>Nym Mixnode</a></span>
+			<span className='node'><img src='../images/oraichain.png'></img><a href='https://oraiscan.io/validators/oraivaloper1xesqr8vjvy34jhu027zd70ypl0nnev5euy9nyl' target='_blank'>Oraichain</a></span>
 		</div>
 
 	</div>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,11 +5,11 @@ return (
 	<div className='forms contact'>
 	<h3>Contact Synergy Nodes</h3>
 		<p><a href="https://www.synergynodes.com" target="_blank">SynergyNodes.com</a></p>
-		<p><a href="https://www.synergynodes.com" target="_blank">Discord</a></p>
-		<p><a href="https://www.synergynodes.com" target="_blank">Telegram</a></p>
+		<p><a href="https://discord.gg/UEppF4sBdM" target="_blank">Discord</a></p>
+		<p><a href="https://t.me/SynergyNodes" target="_blank">Telegram</a></p>
 		<p><a href="https://www.youtube.com/channel/UCFOFPbgP0wLTQ007NtNl_oQ" target="_blank">Youtube</a></p>
-		<p><a href="https://www.synergynodes.com" target="_blank">Github</a></p>
-		<p><a href="https://www.synergynodes.com" target="_blank">CosmosRun.info Explorer</a></p>
+		<p><a href="https://github.com/synergynodes" target="_blank">Github</a></p>
+		<p><a href="https://cosmosrun.info" target="_blank">CosmosRun.info Explorer</a></p>
 	</div>
 );
 };
